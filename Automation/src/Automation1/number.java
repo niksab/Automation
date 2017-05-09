3b63d3455519f78c09a66250a76c19b020323de7
NSabrin <nikhat.sabrin@gmail.com>
2017-05-08 23:04:07 -0400

    First Commit

---
+++ Automation/src/Automation1/number.java
@@ -0,0 +1,24 @@
+package Automation1;
+
+import java.util.StringTokenizer;
+
+public class number {
+public static void main(String[] args){
+
+String str= "  The  with     multiple          spaces   ";
+
+StringTokenizer strTok = new StringTokenizer(str," ");
+StringBuffer strbuf = new  StringBuffer();
+while(strTok.hasMoreTokens())
+{
+	strbuf.append(strTok.nextElement()).append(" ");
+	
+}
+System.out.println(strbuf.toString().trim());
+
+
+}
+
+
+
+}
